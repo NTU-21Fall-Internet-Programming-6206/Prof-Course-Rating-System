47ca4771ce527e53bc755180310727da32b80cd5
MillionBenjain <617572667@qq.com>
2021-10-23 16:53:30 +0800

    update professor CR servlet

---
+++ codes/Backend/src/main/java/org/ci6206/servlet/CourseCRServlet.java
@@ -0,0 +1,19 @@
+package org.ci6206.servlet;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+
+import org.ci6206.service.*;
+import org.ci6206.model.*;
+import org.ci6206.util.JSONUtil;
+
+
+public class CourseCRServlet {
+    
+}
