78a765757be78bd0d891d4d0bd2a893969652cfb
LaChimere <chingkun_lin@126.com>
2021-10-23 15:06:06 +0800

    add: student login

--- codes/Backend/src/main/java/org/ci6206/servlet/StudentLoginServlet.java
+++ codes/Backend/src/main/java/org/ci6206/servlet/StudentLoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 import com.alibaba.fastjson.JSONObject;
+import org.ci6206.dbAPI.TokenDBAO;
 import org.ci6206.service.*;
 import org.ci6206.model.*;
 import org.ci6206.util.JSONUtil;
@@ -18,26 +19,29 @@ public class StudentLoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/json");
-        PrintWriter printWriter = resp.getWriter();
-
         JSONObject msg = JSONUtil.getRequestJson(req);
         if (msg == null) {
             throw new IOException();
         }
+
         String username = msg.getString("username");
         String loginPassword = msg.getString("password");
         try {
+            TokenDBAO con = new TokenDBAO();
+            Token token = con.findTokenWithUsername(username);
+            JSONObject jsonObject = new JSONObject();
+            PrintWriter printWriter = resp.getWriter();
             if (StudentService.login(username, loginPassword)) {
-                JSONObject jsonObject = new JSONObject();
                 jsonObject.put("info", "Success");
-                jsonObject.put("token", TokenUtil.getToken());
+                jsonObject.put("token", token.toString());
                 jsonObject.put("username", username);
-                printWriter.write(jsonObject.toString());
+            } else {
+                jsonObject.put("info", "Failed");
             }
+            printWriter.write(jsonObject.toString());
+            printWriter.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        printWriter.flush();
     }
 }
